6a746109d0b99774b790dab390d4d46250a15bac
Aliaksandr Akhremchyk <Aliaksandr_Akhremhcyk@epam.com>
2020-11-22 17:48:19 +0300

    add basic function

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,39 +1,42 @@
-//make the drop
-const BTN = document.querySelector("button");
-const CALCPANEL = document.querySelector(".calc-panel__battons");
-const CALCSCREEN = document.querySelector(".calc-panel__screen");
-const BTNS = document.querySelectorAll(".calc-panel__battons_batton");
-const SCORE = document.querySelector(".score");
+"use strict";
 
-BTN.addEventListener("mousedown", () => {
-  startGame();
-  //   console.log("херь");
+//make the drop
+var BTN = document.querySelector("button");
+var CALCPANEL = document.querySelector(".calc-panel__battons");
+var CALCSCREEN = document.querySelector(".calc-panel__screen");
+var BTNS = document.querySelectorAll(".calc-panel__battons_batton");
+var SCORE = document.querySelector(".score");
+BTN.addEventListener("mousedown", function () {
+  startGame(); //   console.log("херь");
   //   console.log(currentDrops);
 });
+var FAIL = new Audio("./assets/sounds/fail.mp3");
+var VICTORY = new Audio("./assets/sounds/pobeda.mp3");
+var SEA = new Audio("./assets/sounds/sea.mp3");
 
-const FAIL = new Audio("./assets/sounds/fail.mp3");
-const VICTORY = new Audio("./assets/sounds/pobeda.mp3");
+SEA.onended = function () {
+  return SEA.play();
+};
 
-const SEA = new Audio("./assets/sounds/sea.mp3");
-SEA.onended = () => SEA.play();
-
-const STARTNUMBER = 1;
-const ENDTNUMBER = 2;
-const TIME = 5;
-const TIMEInterval = 1;
-
-let lastLean = 0;
-let currentDrops = [];
-let inxDrop = 0;
-let myevent = new Event("click", { bubbles: true });
-let game = 0;
-let numberOfFeils = 0;
-// clearInterval(autoClick);
+var STARTNUMBER = 1;
+var ENDTNUMBER = 2;
+var TIME = 5;
+var TIMEInterval = 1;
+var lastLean = 0;
+var currentDrops = [];
+var inxDrop = 0;
+var myevent = new Event("click", {
+  bubbles: true
+});
+var game = 0;
+var numberOfFeils = 0; // clearInterval(autoClick);
 // let second = randomAll(STARTNUMBER, ENDTNUMBER);
+
 function startGame() {
   createDrop();
   game = setInterval(createDrop, TIMEInterval * 1000);
 }
+
 function feiled() {
   FAIL.currentTime = 0;
   FAIL.play();
@@ -41,28 +44,29 @@ function feiled() {
   if (++numberOfFeils >= 3) setTimeout(endGame, 800);
   console.log(numberOfFeils);
 }
+
 function endGame() {
   clearInterval(game);
-  const GOAL = document.querySelectorAll(".drop");
-  GOAL.forEach((drop) => drop.remove());
+  var GOAL = document.querySelectorAll(".drop");
+  GOAL.forEach(function (drop) {
+    return drop.remove();
+  });
   numberOfFeils = 0;
 }
+
 function createDrop() {
-  const DROP = document.createElement("div");
-  const DROPcontainer = document.createElement("div");
-  const SPAN1 = document.createElement("span");
-  const SPAN2 = document.createElement("span");
-  const SPAN3 = document.createElement("span");
-  const FILD = document.querySelector(".game__fild");
-
-  const BOTTOM = FILD.offsetHeight;
-  let leans = Math.floor((FILD.offsetWidth - 60) / 80);
-
-  const LEFT = randomLean(leans) * 80 - 60;
-
+  var DROP = document.createElement("div");
+  var DROPcontainer = document.createElement("div");
+  var SPAN1 = document.createElement("span");
+  var SPAN2 = document.createElement("span");
+  var SPAN3 = document.createElement("span");
+  var FILD = document.querySelector(".game__fild");
+  var BOTTOM = FILD.offsetHeight;
+  var leans = Math.floor((FILD.offsetWidth - 60) / 80);
+  var LEFT = randomLean(leans) * 80 - 60;
   DROP.classList.add("drop");
   DROPcontainer.classList.add("drop__container");
-  DROP.style.left = `${LEFT}px`;
+  DROP.style.left = "".concat(LEFT, "px");
   DROP.addEventListener("transitionend", dropTransitionEnd);
 
   function dropTransitionEnd(e) {
@@ -76,28 +80,23 @@ function createDrop() {
   }
 
   SPAN1.classList.add("drop__span");
-
   SPAN2.classList.add("drop__span", "drop__span_big");
   SPAN2.textContent = "+";
   SPAN3.classList.add("drop__span");
-
-  let first = randomAll(STARTNUMBER, ENDTNUMBER);
-  let second = randomAll(STARTNUMBER, ENDTNUMBER);
+  var first = randomAll(STARTNUMBER, ENDTNUMBER);
+  var second = randomAll(STARTNUMBER, ENDTNUMBER);
   SPAN1.textContent = Math.max(first, second);
   SPAN3.textContent = Math.min(first, second);
   DROP.dataset.goal = first + second;
-
   document.body.prepend(DROP);
   DROP.appendChild(DROPcontainer);
   DROPcontainer.appendChild(SPAN1);
   DROPcontainer.appendChild(SPAN2);
   DROPcontainer.appendChild(SPAN3);
-
-  setTimeout(() => {
-    DROP.style.transform = `translateY(${BOTTOM}px)`;
-    DROP.style.transition = `${TIME}s transform ease-in`;
+  setTimeout(function () {
+    DROP.style.transform = "translateY(".concat(BOTTOM, "px)");
+    DROP.style.transition = "".concat(TIME, "s transform ease-in");
   }, 100);
-
   currentDrops[inxDrop] = DROP;
   inxDrop++;
 }
@@ -107,16 +106,20 @@ function randomAll(min, max) {
 }
 
 function randomLean(leans) {
-  const idx = randomAll(1, leans);
+  var idx = randomAll(1, leans);
+
   if (idx === lastLean) {
     return randomLean(leans);
   }
+
   lastLean = idx;
   return idx;
 }
+
 function enterGoal() {
-  const GOAL = document.querySelectorAll(".drop");
-  for (let i = GOAL.length - 1; i >= 0; --i) {
+  var GOAL = document.querySelectorAll(".drop");
+
+  for (var i = GOAL.length - 1; i >= 0; --i) {
     if (GOAL[i].dataset.goal == CALCSCREEN.textContent) {
       SCORE.textContent = +SCORE.textContent + 10;
       GOAL[i].remove();
@@ -128,26 +131,30 @@ function enterGoal() {
 }
 
 function keydown(e) {
-  BTNS.forEach((btn) => {
+  BTNS.forEach(function (btn) {
     if (btn.dataset.code === e.code) {
       btn.dispatchEvent(myevent);
     }
   });
 }
 
-CALCPANEL.addEventListener("click", (e) => {
+CALCPANEL.addEventListener("click", function (e) {
   if (e.target.dataset.key >= 0 && e.target.dataset.key <= 9) {
     CALCSCREEN.textContent += e.target.dataset.key;
   }
+
   if (e.target.dataset.key == "Del") {
     CALCSCREEN.textContent = CALCSCREEN.textContent.slice(0, -1);
   }
+
   if (e.target.dataset.key == "Clear") {
     CALCSCREEN.textContent = "";
   }
+
   if (e.target.dataset.key == "Enter") {
     enterGoal();
   }
+
   e.target.style.backgroundColor = "red";
   setTimeout(removeStyle.bind(e.target), 300, e.target);
 
